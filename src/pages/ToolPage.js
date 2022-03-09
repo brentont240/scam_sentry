8f31475f09c714f5d3912903de1e1ea1b5238cf8
Brenton <brentontreb@gmail.com>
2022-03-08 23:04:00 -0700

    lots of styling and content changes to tools

--- src/pages/ToolPage.js
+++ src/pages/ToolPage.js
@@ -13,7 +13,7 @@ const ToolsPage = () => {
     // if using fetch do npm install --save whatwg-fetch so it can work on ie
     if(tool.InputType === "textarea")
         inputAndButton = <> <textarea name="input" id="input" cols="50" rows="15" className="input-field-color email-detector-text-area" placeholder={tool.InputPlaceholderText} ></textarea> 
-        <span id="button-section"><button type="button" className="btn button-tools" onClick={() => getResult(tool.Id)}>{tool.ButtonText}</button></span> </>;
+        <span id="button-section"><button type="button" className="btn button-tools text-field-button" onClick={() => getResult(tool.Id)}>{tool.ButtonText}</button></span> </>;
     else {
         inputAndButton = <div className="input-group">
         <input name="input" id="input" type={tool.InputType} className="input-field-color input-text form-control" placeholder={tool.InputPlaceholderText} />
@@ -23,15 +23,14 @@ const ToolsPage = () => {
     return ( 
        <div className="container">
         <h1 className="pt-3">{tool.Name}</h1>
-        <div id="results"></div>
+        <section id="results"></section>
             <form>
             {inputAndButton}
             {/* test different button sizes, colors, and other designs */}
             {/* When the page is loading, put a spinner in the button  (bootstap's docs show how to do this)*/}
             {/* <button type="button" className="btn button-tools"  onClick={() => getResult(tool.Id)}>Check</button> */}
-            {/* TESTING DIFFERENT BUTTONS */}
-            {/* use this when it is loading */}
 
+            {/* use this when it is loading */}
             {/* <button className="btn button-tools" type="button" disabled>
             <span className="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
             <span> Loading...</span>
@@ -43,7 +42,9 @@ const ToolsPage = () => {
 };
 
 const getResult = async (id) => {
+    // TODO: maybe reset the results section when the button is click, so the user knows that the results will be for the new input!!!
     const input = document.querySelector("#input").value;
+
     // for the button to load!
     // let button = document.querySelector("#button-section");
     // let buttonBefore = button.innerHTML;
@@ -51,6 +52,21 @@ const getResult = async (id) => {
     // <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
     // <span> Loading...</span>
     // </button>`;
+    let resultsSection = document.querySelector('#results');
+    // if there is no input, show an error message.
+    if (input === ""){
+        resultsSection.innerHTML = `<div class="alert alert-danger">
+        <h1>Error: No input</h1>
+        <p>Please enter valid information into the input field below.</p>
+        </div>`;
+        return;
+    }
+
+    // shows that the results are loading TODO: work on making this look better (see bootstrap spinners for help)
+    // FIXME: is this good when it is centered aligned or should i do something else? should I use a different loading spinner?
+    resultsSection.innerHTML = `<div class="text-center"> <div class="spinner-border tool-spinner" role="status">
+     <span class="visually-hidden">Loading...</span>
+    </div></div>`;
     const options = {
         method: 'POST',
         headers: {
@@ -59,30 +75,46 @@ const getResult = async (id) => {
         },
         body: JSON.stringify({input: input})
     }
-    
+    // TODO: Change the backend link based on the id that is put into here!
     const result = await fetch('https://scam-sentry-backend.herokuapp.com/email-detector', options);       
     const rating = await result.json();
-    
-    console.log(rating.rating)
+    // TODO: FIXME: use rating.isShort to see if the email is short or not!!!
+    console.log(rating.rating);
+    // TODO: implement the use short if the rating is 0 or 1!!!
+    console.log(rating.isShort);
     displayResults(rating.rating);
     // button.innerHTML = buttonBefore;
 };
 
 function displayResults(rating){
+    // TODO: maybe use an icon with these
+    // &#9888 = warning icon!
     let resultsSection = document.querySelector('#results');
+    // TODO: https://getbootstrap.com/docs/5.0/components/alerts/ LOOK AT ALERT COMPONENTS FOR MORE STYLING IDEAS!!!
+    // use icons!!! (maybe use svgs!!!)
     switch(rating){
         case 3:
-            resultsSection.innerHTML = `<h1 class="text-danger">Scam Detected!</h2>
-            <p>This email is definitely a scam</p>`;
+            resultsSection.innerHTML = `<div class="alert alert-danger">
+            <h1>Scam detected!</h1>
+            <p>This email is definitely a scam</p>
+            </div>`;
             return;
         case 2:
-            resultsSection.innerHTML = `<h1 class="text-danger">Scam detected!</h2>`;
+            resultsSection.innerHTML = `<div class="alert alert-danger">
+            <h1>Scam detected!</h1>
+            </div>`;
             return;
+            // TODO: implement the use short if the rating is 0 or 1!!!
         case 1:
-            resultsSection.innerHTML = `<h2 class="text-warning">Possible scam detected.</h2>`;
+            resultsSection.innerHTML = `<div class="alert alert-warning">
+            <h2>Possible scam detected</h2>
+            </div>`;
             return;
+            // TODO: implement the use short if the rating is 0 or 1!!!
         default:
-            resultsSection.innerHTML = `<h2 class="text-success">No scam detected!</h2>`;
+            resultsSection.innerHTML = `<div class="alert alert-success" role="alert">
+            <h2>No scam detected</h2>
+            </div>`;
     }
 }
 

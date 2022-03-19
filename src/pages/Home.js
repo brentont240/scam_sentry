7fc1b9aa4488416a33f6a5d46fc3d4accfb3be23
Brenton <brentontreb@gmail.com>
2022-03-19 16:59:49 -0600

    Lots of work on tools and other various things

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ const Home = () => (
       {/* TODO: put a button here to scroll down? or maybe not? */}
     </div>
     <div className="container mt-4">
-      <h2 className="s-color">A Heading</h2>
+      <h1 className="s-color">Suspect that something is a scam?</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis soluta
         fuga veniam cumque sapiente necessitatibus, neque explicabo voluptates,
@@ -22,32 +22,16 @@ const Home = () => (
         praesentium tempore at, consequuntur, maxime ipsum delectus id soluta
         veniam. Itaque sequi qui libero id fuga. Reprehenderit!
       </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis soluta
-        fuga veniam cumque sapiente necessitatibus, neque explicabo voluptates,
-        nulla ad vitae! Id consequatur fuga rem provident odio illum nobis.
-        Ratione repellat sapiente consequatur reprehenderit aliquam labore
-        molestiae quisquam repellendus doloremque, in amet et nobis, numquam
-        modi explicabo blanditiis aut nulla? Ipsum nulla suscipit nesciunt
-        labore quam neque obcaecati, soluta rem, aliquam officia voluptatibus
-        debitis ab sequi, ut dolores vitae fugiat expedita. Rem beatae officia
-        inventore expedita ipsa iste eius quidem consequatur rerum saepe
-        praesentium tempore at, consequuntur, maxime ipsum delectus id soluta
-        veniam. Itaque sequi qui libero id fuga. Reprehenderit!
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis soluta
-        fuga veniam cumque sapiente necessitatibus, neque explicabo voluptates,
-        nulla ad vitae! Id consequatur fuga rem provident odio illum nobis.
-        Ratione repellat sapiente consequatur reprehenderit aliquam labore
-        molestiae quisquam repellendus doloremque, in amet et nobis, numquam
-        modi explicabo blanditiis aut nulla? Ipsum nulla suscipit nesciunt
-        labore quam neque obcaecati, soluta rem, aliquam officia voluptatibus
-        debitis ab sequi, ut dolores vitae fugiat expedita. Rem beatae officia
-        inventore expedita ipsa iste eius quidem consequatur rerum saepe
-        praesentium tempore at, consequuntur, maxime ipsum delectus id soluta
-        veniam. Itaque sequi qui libero id fuga. Reprehenderit!
-      </p>
+      {/* TODO: look at better ways to word / format this!!! */}
+      {/* TODO: DO I WANT TO USE THIS QUOTE FORMAT???!!! */}
+      <h3>&#8595; might want to change this &#8595;</h3>
+      <div className="quote-div">
+      <h4><b>Keep in mind:</b></h4>
+      <blockquote className="blockquote">
+        <p>If it sounds too good to be true, it probably is.</p>
+      </blockquote>        
+      </div>
+
       {/* TODO: put sections for the tools page and articles page here!!! */}
     </div>
   </>

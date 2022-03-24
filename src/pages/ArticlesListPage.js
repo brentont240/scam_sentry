b2c737b932e778b96dd7a6923bb2acab7555200b
Brenton <brentontreb@gmail.com>
2022-03-23 22:41:52 -0600

    working on articles

--- src/pages/ArticlesListPage.js
+++ src/pages/ArticlesListPage.js
@@ -5,23 +5,38 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
 
 const ArticlesListPage = () => (
-    <div className="container">
+  <div className="container">
       {/* TODO: maybe use a card component for this? */}
-      <h1 className="pt-3">Articles</h1>
+      {/* TODO: MAKE THIS HEADER LOOK BETTER? */}
+      <h1 className="pt-4">Articles</h1>
       {/* create an article component then generate them with a map or something!!! */}
     <div className="article-list">
       {Articles.List.map((article, key) => (
         <div className="article-item" key={key }>
-          <img src={require(`../images/${article.Image}.png`)} alt ="stuff" className="img-thumbnail rounded article-thumbnail-image"></img>
-          <div>
+          {/* TODO: ADD ALT TEXT to the images? */}
+          <Link to={`/articles/${article.Url}`}>
+          <img src={require(`../images/${article.Image}.png`)} alt={article.ImageAlt} className="img-thumbnail rounded article-thumbnail-image"></img>
+          </Link>
+          {/*  TODO: DECREASE THE GAP ON MOBILE  */}
+          <div className="article-details">
             <h3>{article.ArticleHeading}</h3>
             <p>Type: <span className="article-type">{article.Type}</span></p>
-            <p>{article.Content.substring(0,200)}... <Link to={`/articles/${article.Url}`}>Read More <FontAwesomeIcon icon={faArrowRight} /></Link></p>
+            {/* TODO: Maybe make it so that this changes when the window is resized? */}
+            <p>{article.Content.substring(0,getSubstringLength())}... <Link to={`/articles/${article.Url}`}>Read More <FontAwesomeIcon icon={faArrowRight} /></Link></p>
           </div>
         </div>        
       ))}
     </div>
     </div>
 );
+
+// displays a different amount of preview text, depending on the screensize
+function getSubstringLength(){
+  const screensize = window.innerWidth;
+  if(screensize < 770){
+    return 200;
+  }
+  return 500;
+}
 
 export default ArticlesListPage;
--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -1,7 +1,10 @@
 import React from "react";
 import articles from '../data/articles.json';
 import PageNotFound from "./PageNotFound";
+import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 const ArticlesPage = () => {
     let params = useParams();
@@ -10,16 +13,16 @@ const ArticlesPage = () => {
 
     return ( 
        <div className="container pt-4">
-        <p className="text-muted">Add a go back to articles button?</p>
+        <Link to={'/articles-list'}><FontAwesomeIcon icon={faArrowLeft} /> Go back to article list</Link>
+        <p><b>Do i want this go back button?</b></p>
         <h1>{article.ArticleHeading}</h1>
         <h2>{article.ArticleSubHeading}</h2>
         <p>center this image, make it look better, and also add alt text</p>
-        <p>do good styling for this, look up what looks good!!!</p>
-        <p>give credit to images somehow</p>
-        <img src={require(`../images/${article.Image}.png`)} alt="" className="article-image"/>
-        <p className="my-4">{article.Content}</p>
+        <img src={require(`../images/${article.Image}.png`)} alt={article.ImageAlt} className="article-image"/>
+        <div className="my-4" dangerouslySetInnerHTML={ {__html: article.Content} }></div>
         </div>
     );
 };
+
 
 export default ArticlesPage;
